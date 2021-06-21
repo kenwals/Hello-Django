730d313d163e6ab8202bd30b321f1e5416f0c6ca
kenwals <kenwals@gmail.com>
2021-06-21 18:18:48 +0000

    allow host setting

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-p=lz!9a82bzh+z144s#&&!)##+w(itxfcx(7&@$8k#(33%6bcj
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['kw-django-todo-app.herokuapp.com']
 
 
 # Application definition
