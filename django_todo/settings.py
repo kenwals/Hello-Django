a704f22ffdad90164b55e2cf5d355eaaaa890e7c
kenwals <kenwals@gmail.com>
2021-06-22 12:45:45 +0100

    fix secret keys split and reset

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-p=lz!9a82bzh+z144s#&&!)##+w(itxfcx(7&@$8k#(33%6bcj')
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = development
